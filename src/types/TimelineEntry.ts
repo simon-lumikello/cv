43355aea54278cb4a5d7835674de5801610d9b03
Simon Lumikello <simon.lumikello@gmail.com>
2025-12-30 11:07:48 +0200

    run biome check on hte project
    - organize imports
    - more consistent whitespaces

--- src/types/TimelineEntry.ts
+++ src/types/TimelineEntry.ts
@@ -1,11 +1,11 @@
 export interface TimelineEntry {
-    yearStart: number
-    yearEnd?: number
-    ongoing?: boolean
-    title: string
-    description: string
-    company: string
-    location: string
-    technologies: string[]
-    pageBreak?: boolean
+	yearStart: number;
+	yearEnd?: number;
+	ongoing?: boolean;
+	title: string;
+	description: string;
+	company: string;
+	location: string;
+	technologies: string[];
+	pageBreak?: boolean;
 }
